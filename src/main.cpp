908ec3533f4ee42b477740c23913dcccd33c090f
JOHN LAWRENCE ABENOJA <jlabenoja28@gmail.com>
2026-01-01 16:46:02 +0800

    add build folder

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,13 @@
 using namespace std;
 
 int main(){
-    cout<<" --- FLAVOORTOWN RPG START ---"<<endl;
+    cout<<" --- FLAVORTOWN RPG START ---"<<endl;
 
     Entity goblin("Spicy goblin", 50, 5);
 
     cout<<"A Wild "<< goblin.name <<" appears!" <<endl;
     cout<<"It has "<<goblin.health << " HP." <<endl;
 
-        cout<<"Is the goblin alive? "<<endl;
-    if(goblin.isAlive() == true){
-        cout<<"Yes it is alive!"<<endl;
-    }
-    else
-        cout<<"Oh, it's dead!"<<endl;
 
-    goblin.takeDamage(50);
-
-    cout<<"It has "<<goblin.health << " HP." <<endl;
-
-    cout<<"Is the goblin alive? "<<endl;
-    if(goblin.isAlive() == true){
-        cout<<"Yes it is alive!"<<endl;
-    }
-    else
-        cout<<"Oh, it's dead!"<<endl;
 }
 
